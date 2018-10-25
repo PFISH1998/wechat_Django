2c1dad441f2e59642b97bede4d373a21f043b4d3
pfish <perry1998@outlook.com>
2018-10-25 17:05:21 +0800

    更改数据库，加入 circle app

--- wechat/settings.py
+++ wechat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'search',
     'news',
+    'circle',
     # 'django_crontab',
 ]
 
@@ -87,10 +88,10 @@ DATABASES = {
     #     'HOST':'127.0.0.1',
     #     'PORT':'3306',
     # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
