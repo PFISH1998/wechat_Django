2593b461ada7efb4fd82260c9d96be00cdc00748
pfish <perry1998@outlook.com>
2018-11-14 20:37:51 +0800

    更新圈子models

--- circle/models.py
+++ circle/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user_id = models.IntegerField()
+    nick_name = models.CharField(max_length=20)
+    content = models.TextField()
+    picture = models.FileField()
+    pub_time = models.DateTimeField()
+    display = models.BooleanField()
+
+
+class CircleUser(models.Model):
+    pass
+
+
