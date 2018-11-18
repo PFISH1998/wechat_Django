a1a77c63a2543c97eb8679d9357be3c71192bf3c
pfish <perry1998@outlook.com>
2018-11-18 18:02:17 +0800

    增加URL

--- circle/urls.py
+++ circle/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 
 app_name = 'circle'
 urlpatterns = [
+    path('post/', views.PostList.as_view()),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
+
+    path('user/', views.CircleUserList.as_view()),
+    path('user/<int:pk>/', views.CircleUserDetail.as_view()),
+
+    path('comments/<int:pk>/', views.CommentsList.as_view()),
+
+    path('like/<int:pk>/<int:pk/')
 
 ]
