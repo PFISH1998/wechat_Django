51101601e75ae9c8629ded922addcb9467f5c6be
pfish <perry1998@outlook.com>
2018-11-19 22:03:09 +0800

    点赞和其他功能查询机制

--- circle/views.py
+++ circle/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from circle.serializers import PostSerializers, CircleUserSerializer, CommentsSerializer
-from circle.models import Post, CircleUser, Comments
+from circle.serializers import PostSerializers, CircleUserSerializer, CommentsSerializer, LikeSerializer
+from circle.models import Post, CircleUser, Comments, Like
 
 
 class PostList(APIView):
@@ -26,15 +26,24 @@ class PostDetail(APIView):
         try:
             return Post.objects.get(pk=pk, display=True)
         except Post.DoesNotExist:
-            return HttpResponse(status=404)
+            print("Post DoesNotExist!")
+            raise
 
     def get(self, request, pk):
-        post = self.get_object(pk)
+        try:
+            post = self.get_object(pk)
+        except Post.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
         serializer = PostSerializers(post)
         return JsonResponse(serializer.data, safe=False)
 
     def put(self, request, pk):
-        post = self.get_object(pk)
+        try:
+            post = self.get_object(pk)
+        except Post.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
         serializer = PostSerializers(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -54,11 +63,14 @@ class CircleUserList(APIView):
         print(request.data)
         print(request.data['nick_name'])
         circle_user = CircleUser.objects.filter(nick_name=request.data['nick_name'])
-        if circle_user:
-            return Response(circle_user, status=status.HTTP_304_NOT_MODIFIED)
+        print(circle_user)
+        if circle_user.exists():
+            print("not")
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
 
         serializer = CircleUserSerializer(data=request.data)
         if serializer.is_valid():
+            print('yes')
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_304_NOT_MODIFIED)
@@ -75,10 +87,15 @@ class CircleUserDetail(APIView):
         try:
             return CircleUser.objects.get(pk=pk)
         except CircleUser.DoesNotExist:
-            return HttpResponse(status=404)
+            print('CircleUser.DoesNotExist')
+            raise
 
     def put(self, request, pk):
-        circle_user = self.get_object(pk)
+        try:
+            circle_user = self.get_object(pk)
+        except CircleUser.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = CircleUserSerializer(circle_user, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -86,7 +103,11 @@ class CircleUserDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, pk):
-        circle_user = self.get_object(pk)
+        try:
+            circle_user = self.get_object(pk)
+        except CircleUser.DoesNotExist:
+            return HttpResponse(status=404)
+
         serializer = CircleUserSerializer(circle_user)
         return JsonResponse(serializer.data)
 
@@ -97,30 +118,29 @@ class CommentsList(APIView):
         try:
             return Post.objects.get(display=True, pk=pk)
         except Post.DoesNotExist:
-            raise Exception('no result')
-
+            print('Post DoesNotExist!')
+            raise
 
     def get_object(self, pk):
         try:
             self.get_post(pk)
             return Comments.objects.filter(circle=pk)
-        except Comments.DoesNotExist:
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        except Comments.DoesNotExist or Post.DoesNotExist:
+            raise
 
     def get(self, request, pk):
         try:
             comments = self.get_object(pk)
-        except Exception as e:
-            print(e)
+        except Post.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
         serializer = CommentsSerializer(comments, many=True)
         return Response(serializer.data)
 
     def post(self, request, pk):
         try:
             post = self.get_post(pk)
-        except Exception as e:
-            print(e)
+        except Post.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
         serializer = CommentsSerializer(data=request.data)
@@ -131,7 +151,41 @@ class CommentsList(APIView):
         return HttpResponse(status=status.HTTP_304_NOT_MODIFIED)
 
 
-class Like(APIView):
-    def post(self, request, user, circle):
-        pass
+class LikeList(APIView):
+
+    def post(self, request, pk):
+        try:
+            post = Post.objects.get(display=True, pk=pk)
+            user_id = request.data['nick_name']
+            like = Like.objects.filter(nick_name_id=user_id, type_id=pk, type=1)
+            like_status = True
+            if like.exists():  # 不存在直接插入数据。存在既更新，将点赞状态取反
+                for i in like:
+                    like_status = not i.status
+                like.update(status=like_status)
+                post.change_like(like_status)
+                post.save()
+                return HttpResponse(status=status.HTTP_201_CREATED)
+
+            Like.objects.create(nick_name_id=user_id, type_id=pk, type=1)
+            post.change_like(like_status)
+            post.save()
+            return HttpResponse(status=status.HTTP_201_CREATED)
+
+        except Post.DoesNotExist:
+            # print(e)
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk):
+        try:
+            like = Like.objects.get(type_id=pk)
+        except Like.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = LikeSerializer(like)
+        return Response(serializer.data)
+
+
+
+
 
