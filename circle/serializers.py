b9a4b1cd79698e889871dbbc0fb1d93867bf1c32
pfish <perry1998@outlook.com>
2018-11-20 22:08:57 +0800

    增加Post序列化时的字段

--- circle/serializers.py
+++ circle/serializers.py
@@ -3,9 +3,14 @@ from circle.models import CircleUser, Post, Comments, Like
 
 
 class PostSerializers(serializers.ModelSerializer):
+    pub_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True, required=False)
+    comments_count = serializers.IntegerField(read_only=True, required=False)
+    like_count = serializers.IntegerField(read_only=True, required=False)
+    post_user = serializers.CharField(source='uid.nick_name', required=False)
+
     class Meta:
         model = Post
-        fields = ('id', 'nick_name', 'content',
+        fields = ('id', 'post_user', 'uid', 'content',
                   'pub_time', 'comments_count', 'like_count', 'dis_like_count')
 
     def create(self, validated_data):
@@ -16,15 +21,19 @@ class PostSerializers(serializers.ModelSerializer):
 
 
 class CircleUserSerializer(serializers.ModelSerializer):
+    uid = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = CircleUser
-        fields = ('nick_name', 'we_name', 'head_pic', 'description')
+        fields = ('uid', 'nick_name', 'we_name', 'head_pic', 'description')
 
     def create(self, validated_data):
         return CircleUser.objects.create(**validated_data)
 
 
 class CommentsSerializer(serializers.ModelSerializer):
+    comment_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True, required=False)
+
     class Meta:
         model = Comments
         fields = ('id', 'comment_content', 'circle', 'comment_time', 'from_user')
@@ -33,7 +42,13 @@ class CommentsSerializer(serializers.ModelSerializer):
         return Comments.objects.create(**validated_data)
 
 
+# class NotesSerializer(serializers.ModelSerializer):
+#     comment_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True, required=False)
+
+
+
 class LikeSerializer(serializers.ModelSerializer):
+    like_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True, required=False)
     class Meta:
         model = Like
         fields = ('id', 'type_id', 'type', 'nick_name', 'like_time')
