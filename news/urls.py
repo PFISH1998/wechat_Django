80ec2ce434ad548111ea79e515362f6dfcebea6b
pfish <perry1998@outlook.com>
2018-12-29 22:01:42 +0800

    优化新闻页面

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 app_name = 'news'
 urlpatterns = [
-    path('news', views.content, name='content'),
-    path('list', views.news_list, name='news_list')
+    path('news/', views.content, name='content'),
+    path('list/', views.news_list, name='news_list')
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,6 @@
-import gc
-
-from django.shortcuts import render
 from django.http import HttpResponse
 from tools.searchNews import GetNews
-from tools.news import get_url_from_index_page
+from tools.news import get_url_from_index_page, get_total_content
 import json
 # Create your views here.
 
@@ -13,11 +10,14 @@ g = GetNews()
 def content(request):
     # print(request.GET['content'])
     url = request.GET['content']
-    # print(url)
+    print(url)
     # try:
-    news_content = g.get_page_content(url)
+    # news_content = g.get_page_content(url)
+
+    news_content, pic_list = get_total_content(url)
     return HttpResponse(json.dumps({
-        'news_content': news_content
+        'news_content': news_content,
+        'pic_list': pic_list
     }))
     # except Exception as e:
     # print(e)
@@ -32,32 +32,35 @@ def news_list(request):
     page_type = request.GET['type']
     print(page_type)
     print(page_num)
-    if page_num != '1':
-        page_type = page_num
-    try:
-        data = get_url_from_index_page(page_type)
-        # print(result_list)
-        return HttpResponse(json.dumps({
-            'news_list': data
-        }))
-    except AttributeError:
-        return HttpResponse(
-            json.dumps({
-            'page_num': page_num,
-        }))
-    except ValueError:
-        return HttpResponse(
-            json.dumps({
-            'page_num': page_num,
-            }))
-    except BaseException as e:
-        print(e)
-        return HttpResponse(
-            json.dumps({
-                'page_num': page_num,
-            }))
-    finally:
-        print('完成')
+    if page_num == '0':
+        page = page_type
+    # try:
+    else:
+        page = '{}/{}'.format(page_type, page_num)
+    data = get_url_from_index_page(page)
+    return HttpResponse(json.dumps({
+        'news_list': data
+    }))
+    # except AttributeError:
+    #     print('AttributeError')
+    #     return HttpResponse(
+    #         json.dumps({
+    #         'page_num': page_num,
+    #     }))
+    # except ValueError:
+    #     print('ValueError')
+    #     return HttpResponse(
+    #         json.dumps({
+    #         'page_num': page_num,
+    #         }))
+    # except BaseException as e:
+    #     print(e)
+    #     return HttpResponse(
+    #         json.dumps({
+    #             'page_num': page_num,
+    #         }))
+    # finally:
+    #     print('完成')
         # gc.collect()
 
 
