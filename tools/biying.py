0eeedb8e93e901eb410e889a6dfc33d59babbe33
pfish <perry1998@outlook.com>
2019-01-24 15:27:04 +0800

    bing api

--- tools/biying.py
+++ tools/biying.py
@@ -4,7 +4,7 @@ import time
 
 
 def request_api():
-    api_url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    api_url = 'https://www2.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
     r = requests.get(api_url)
     image = json.loads(r.text)
     daily = structure_data(image)
@@ -14,7 +14,7 @@ def request_api():
 def structure_data(image):
     daily = dict()
     d = time.strftime("%b %d %Y", time.localtime()).split(' ')
-    daily['pic_url'] = 'http://s.cn.bing.net' + image['images'][0]['url']
+    daily['pic_url'] = 'http://www2.bing.com' + image['images'][0]['url']
     daily['content'] = image['images'][0]['copyright']
     daily['mon'] = d[0]
     daily['day'] = int(str(d[1]))
