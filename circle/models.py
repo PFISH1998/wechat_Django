5e652d71f47005153fefd2502ec219d122cb3fa5
pfish <perry1998@outlook.com>
2019-02-05 22:50:44 +0800

    增加图片字段

--- circle/models.py
+++ circle/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):  # 发布圈子
     uid = models.ForeignKey('CircleUser', on_delete=models.DO_NOTHING)  # 发布的用户昵称
     content = models.TextField()  # 内容
-    # picture = models.TextField(null=True, default='')  # 发布图片
+    picture = models.TextField(null=True, blank=True)  # 发布图片
     pub_time = models.DateTimeField(auto_now_add=True)  # 发布时间
     type = models.CharField(choices=
                             (('normal', '普通'), ('top', '置顶'), ('note', '通知')),
@@ -56,6 +56,7 @@ class Comments(models.Model):  # 评论表
     # display = models.ForeignKey(to='Post', to_field='display')
     # replay_user = models.OneToOneField()
     # view = models
+
 
 class Like(models.Model):  # 点赞表
     post = models.ForeignKey('Post', on_delete=models.DO_NOTHING)  # 点赞对象的 ID
