11f55b7c9f175f5d08ee3b540d1cb70eb24e1d27
pfish <perry1998@outlook.com>
2019-02-10 14:42:35 +0800

    Update tests.py

--- circle/tests.py
+++ circle/tests.py
@@ -1,3 +1,10 @@
+# import time
+#
 from django.test import TestCase
-
-# Create your tests here.
+#
+# # Create your tests here.
+# import uuid
+# import random
+# from qiniu import Auth, put_data, etag, put_file
+# import qiniu.config
+#
