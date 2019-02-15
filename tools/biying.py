e4e59a3a1af903281c2768bb3d09601e796e00c5
pfish <perry1998@outlook.com>
2019-02-15 14:27:56 +0800

    修改url

--- tools/biying.py
+++ tools/biying.py
@@ -4,7 +4,7 @@ import time
 
 
 def request_api():
-    api_url = 'https://www2.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    api_url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
     r = requests.get(api_url)
     image = json.loads(r.text)
     daily = structure_data(image)
@@ -14,9 +14,17 @@ def request_api():
 def structure_data(image):
     daily = dict()
     d = time.strftime("%b %d %Y", time.localtime()).split(' ')
-    daily['pic_url'] = 'https://www2.bing.com' + image['images'][0]['url']
+    daily['pic_url'] = 'https://cn.bing.com' + image['images'][0]['url']
     daily['content'] = image['images'][0]['copyright']
     daily['mon'] = d[0]
     daily['day'] = int(str(d[1]))
     daily['year'] = d[2]
     return daily
+
+# class Biying:
+#     def __init__(self):
+#         self.base_url = "https://cn.bing.com"
+#         self.api = "/HPImageArchive.aspx?format=js&idx=0&n=1"
+#
+#
+#     def
